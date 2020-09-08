38274c669dbfd830fe476a62279ca143a931edc7
aauria <afauria@est.itssg.edu.ec>
2020-09-08 00:13:00 -0500

    octavo_respaldo

---
+++ usuario/views.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from usuario.forms import RegistroForms
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from core import views
+
+
+class registro_usuario(CreateView):
+    model = User
+    template_name = "registrar_usuario.html"
+    form_class = RegistroForms
+    success_url = reverse_lazy(views.home)
+
+# Create your views here.
